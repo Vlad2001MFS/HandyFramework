37b366f07a3188f9b460910c57b302953174c3a9
Vlad2001MFS <kopytin.vladislav2001@yandex.ru>
2019-07-03 22:10:11 +0300

    fix: returning an invalid pointer instead native window handle

--- src/hd/System/hdOSSpecificInfo.cpp
+++ src/hd/System/hdOSSpecificInfo.cpp
@@ -1,4 +1,5 @@
 #include "hdOSSpecificInfo.hpp"
+#include "../Core/hdCommon.hpp"
 #include "../hdBuildConfig.hpp"
 #include "../../3rd/include/SDL2/SDL_syswm.h"
 
@@ -6,7 +7,10 @@ namespace hd {
 
 void *getSystemWindowHandle(void *sdlWindow) {
     SDL_SysWMinfo info;
-    SDL_GetWindowWMInfo(static_cast<SDL_Window*>(sdlWindow), &info);
+    SDL_VERSION(&info.version);
+    if (!SDL_GetWindowWMInfo(static_cast<SDL_Window*>(sdlWindow), &info)) {
+        HD_LOG_ERROR("Failed to get native window handle. Error:\n%s", SDL_GetError());
+    }
 #if defined(HD_PLATFORM_UNIX)
     return reinterpret_cast<void*>(info.info.x11.window);
 #elif defined(HD_PLATFORM_WIN)
